b09e98b7054080292117e4e785aad339998ad0b5
Sebastian-IngSoft <sebastiansarmientocas@gmail.com>
2024-08-02 19:39:11 -0500

    layouts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body>
+@extends('layouts.app')
+@section('title','laravel 11')
+
+@push('css') {{--se puede invocar varias veces y suma el contenido no cmbia--}}
+    <style>
+        body{
+            background-color: black
+        }
+    </style>
+@endpush
+
+
+@section('content')
     <div class="max-w-4xl mx-auto px-4">
 
         welcome to the home with blade
 
-        <x-alert2 type="info" class="mb-4" class2="mx-1">{{--tiene que haber la carpeta componentes para que sea usado
-            --}}
-            <x-slot name="title"> 
+        <x-alert2 type="info" class="mb-4" class2="mx-1">{{-- tiene que haber la carpeta componentes para que sea usado --}}
+            <x-slot name="title">
                 titulo con variable
             </x-slot>
 
-            Contenido de la alerta
+            Contenido con layouts
         </x-alert2>
-    </div>
-</body>
-</html>
+@endsection
